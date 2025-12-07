cd953dc3d75c22bdec1118474f061588f48f6320
noam9472-gif <noam9472@gmail.com>
2025-12-07 15:02:44 +0200

    שדג

--- MyProjectsAPI/Controllers/SelectController.cs
+++ MyProjectsAPI/Controllers/SelectController.cs
@@ -17,6 +17,66 @@ namespace MyProjectsAPI.Controllers
             CountriesList countriesList = countriesDB.SelectAll();
             return countriesList;
         }
+
+        [HttpGet]
+        [ActionName("ContinentsSelector")]
+
+        public ContinentsList SelectAllContinents()
+        {
+            ContinentsDB continentsDB = new ContinentsDB();
+            ContinentsList continentsList = continentsDB.SelectAll();
+            return continentsList;
+        }
+
+        [HttpGet]
+        [ActionName("LanguagesSelector")]
+
+        public LanguagesList SelectAllLanguages()
+        {
+            LanguagesDB languagesDB = new LanguagesDB();
+            LanguagesList languagesList = languagesDB.SelectAll();
+            return languagesList;
+        }
+
+        [HttpGet]
+        [ActionName("WeatherSelector")]
+
+        public WeatherList SelectAllWeather()
+        {
+            WeatherDB weatherDB = new WeatherDB();
+            WeatherList weatherList = weatherDB.SelectAll();
+            return weatherList;
+        }
+
+        [HttpGet]
+        [ActionName("UserDetailsSelector")]
+
+        public UserDetailsList SelectAllUserDetails()
+        {
+            UserDetailsDB userDetailsDB = new UserDetailsDB();
+            UserDetailsList userDetailsList = userDetailsDB.SelectAll();
+            return userDetailsList;
+        }
+
+        [HttpGet]
+        [ActionName("CategorySelector")]
+
+        public CategoryList SelectAllCategory()
+        {
+            CategoryDB categoryDB = new CategoryDB();
+            CategoryList categoryList = categoryDB.SelectAll();
+            return categoryList;
+        }
+
+        [HttpGet]
+        [ActionName("AttractionsSelector")]
+
+        public AttractionsList SelectAllAttractions()
+        {
+            AttractionsDB attractionsDB = new AttractionsDB();
+            AttractionsList attractionsList = attractionsDB.SelectAll();
+            return attractionsList;
+        }
     }
 //    {
 //        public IActionResult Index()
